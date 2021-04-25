2fed7dac4716a68e00ceb9a0cefea2f1b279bed3
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2021-04-25 20:45:38 +0400

    feature: remove fakeResponseIsNetworkError and add dispatchReturns

--- src/type.ts
+++ src/type.ts
@@ -1,13 +1,20 @@
 import { IStringifyOptions } from 'qs'
 
 declare module '@savchenko91/rc-redux-api-mw/dist/type.d' {
-  export interface APIAction<ResponseBody = unknown, RequestBody = unknown, Payload = unknown>
-    extends Omit<RequestInit, 'headers' | 'body'> {
+  type ActionName = 'endAction' | 'APIAction'
+
+  export interface APIAction<
+    ResponseBody = unknown,
+    RequestBody = unknown,
+    Payload = unknown,
+    DispatchReturns extends ActionName = 'APIAction'
+  > extends Omit<RequestInit, 'headers' | 'body'> {
     url: string
     type: string
     returnResponse?: boolean
     headers?: APIHeaders
     body?: RequestBody
+    dispatchReturns?: DispatchReturns
     responseBodyType?: ResponseBodyType
     stageActionTypes: StageActionTypes
     onStart?: OnStart<ResponseBody, RequestBody, Payload>
@@ -15,7 +22,6 @@ declare module '@savchenko91/rc-redux-api-mw/dist/type.d' {
     onFail?: OnFail<ResponseBody, RequestBody, Payload>
     payload?: Payload
     fakeResponse?: Response
-    fakeResponseIsNetworkError?: boolean
     fakeResponseTimeout?: number
   }
 }
